8cef45a16dd63ba83c5bc4f8a0e7f5e6892a4af3
hanhyunsoo <gustn4563@gmail.com>
2019-07-20 01:49:44 +0900

    Create User sign up

--- account/views.py
+++ account/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+from .models import User
+
 
 # Create your views here.
+def sign_up(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('account:sign_up')
+    else:
+        form = SignUpForm()
+        print(form)
+
+    return render(request, 'account/sign_up.html', {'form': form})
+
