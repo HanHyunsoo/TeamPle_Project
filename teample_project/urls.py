3f3e4734e37a51382c08a61e5536cbdb880c24ac
SinHaecheol <since19990820@gmail.com>
2019-08-02 18:35:22 +0900

    add haecheol

--- teample_project/urls.py
+++ teample_project/urls.py
@@ -21,4 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
+    path('mail/', include('mail.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
