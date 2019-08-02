fa47783c615df0fac627ff628b13eec5812ac834
hanhyunsoo <gustn4563@gmail.com>
2019-08-02 19:15:05 +0900

    Modified urls.py

--- account/urls.py
+++ account/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 app_name = 'account'
 urlpatterns = [
-    path('sign-up/', views.sign_up, name='sign_up'),
-    path('sign-in/', views.sign_in, name='sign_in'),
-    path('sign-out/', views.sign_out, name='sign_out'),
-    path('user-home/<int:user_id>', views.user_home, name='user_home'),
+    path('sign_up/', views.sign_up, name='sign_up'),
+    path('sign_in/', views.sign_in, name='sign_in'),
+    path('sign_out/', views.sign_out, name='sign_out'),
+    path('user_home/<int:user_id>', views.user_home, name='user_home'),
 ]
