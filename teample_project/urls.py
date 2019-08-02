9c4bfd6b361defb40d5b7d8fc96372f5d80d7d62
soohyunkim <sss4920@likelion.org>
2019-08-02 11:07:19 +0900

    mainpage

--- teample_project/urls.py
+++ teample_project/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+import main.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
+    path('', main.views.home, name='home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
