3a6515de35dbabeef77da6e16849adc72c0b581c
hanhyunsoo <gustn4563@gmail.com>
2019-08-08 18:16:11 +0900

    Add setting schedule

--- account/urls.py
+++ account/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('sign_out/', views.sign_out, name='sign_out'),
     path('user_home/<int:user_pk>', views.user_home, name='user_home'),
     path('user_info/<int:user_pk>', views.user_info, name='user_imfo'),
+    path('schedule/', views.set_schedule, name='set_schedule'),
 ]
